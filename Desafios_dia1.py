87fb5f77fddad9cd57386f803c848a5a9e9855eb
Levi46172993 <Clevifranca7@gmail.com>
2025-05-08 17:58:30 -0300

    ...

--- Desafios_dia1.py
+++ Desafios_dia1.py
@@ -1,56 +1,35 @@
-# #desafio 1
-# n1 = int(input('Digite um número: '))
-# print(f'O antecessor de {n1} é {n1 - 1} e o seu sucessor é {n1 + 1 }')
+#desafio 12
+# from math import floor
+# num = float(input('Digite um número com vírgula: '))
+# Inteiro = floor(num)
+# print(f'A parte inteira de {num} é {Inteiro} ')
 
-# #desafio 2
-# n1 = int(input('Digite um número: '))
-# print(f'O dobro de {n1} é {n1 * 2 } o seu triplo {n1 * 3 }\n e a sua raiz quadrada é {n1 **(1/2)} ')
+#desafio 13
+from math import sqrt
+cateto_oposto = float(input('Comprimento do cateto oposto: '))
+cateto_adjacente = float(input('Comprimento do cateto adjacente: '))
+hipotenusa = (cateto_oposto ** 2) + (cateto_adjacente ** 2)
+hipotenusa = sqrt(hipotenusa)
+print(f'A hipotenusa vai medir {hipotenusa:.2f}')
 
-# #desafio 3
-# n1 = float(input('Digite sua primeira nota: '))
-# n2 = float(input('Digite sua segunda nota: '))
-# print(f'A média entre {n1} e {n2} é {(n1 + n2) / 2 }')
+#desafio 14
+#não sei fazer
+pass
 
-# # desafio 4 
-# n1 = int(input('Digite uma distância em metros: '))
-# print(f'Está distância convertida em centímetros é {n1 * 100 }cm\n e essa distância convertida em mílimetros é {n1 * 1000}mm')
+# #desafio 15
+# print('O professor quer que um dos alunos apague o quadro.\nEntre Luiz, Giovanna, Pedro e lucas oq irá apagar o quadro vai  ser o...')
+# from random import randint 
+# pedro = 'Pedro'
+# luiz = 'Luiz'
+# guilherme = 'Guilherme'
+# lucas = 'Lucas'
+# print(randint(pedro , luiz , guilherme , lucas))
+pass
 
-# # desafio 5
-# n1 = int(input('Vou falar a tabuada do número que digitar: '))
-# print( f'A tabuada do {n1} está abaixo' )
-# print(f'{n1} x 1 = {n1 * 1}')     ,    print(f'{n1} x 2 = {n1 * 2}')
-# print(f'{n1} x 3 = {n1 * 3}')     ,    print(f'{n1} x 4 = {n1 * 4}')
-# print(f'{n1} x 5 = {n1 * 5}')     ,    print(f'{n1} x 6 = {n1 * 6}')
-# print(f'{n1} x 7 = {n1 * 7}')     ,    print(f'{n1} x 8 = {n1 * 8}')
-# print(f'{n1} x 9 = {n1 * 5}')     ,    print(f'{n1} x 10 = {n1 * 10}')
+#desafio 16
+#não sei fazer 
+pass    
 
-# # desafio 6
-# carteira = float(input('Digite quantos reais você tem em sua carteira:R$'))
-# print(f'Com R${carteira} você conseguiria comprar US${carteira / 3.27:.2f}')
-
-# # desafio 7
-# parede_altura = float(input('Digite a alutura da parede que quer pintar: '))
-# parede_largura = float(input('Digite a largura da parede que quer pintar: '))
-# area = parede_altura * parede_largura
-# print(f'A aréa dessa parede é igual a {area}m² e se cada litro de tinta cobre\nUma aréa de 2m², então você precisará de { area / 2 }L de tinta')
-
-# desafio 8
-# preco = int(input('Qual o preço do produto que deseja comprar?: '))
-# porcentagem = preco - (preco * 5 / 100)
-# print(f'Estamos com uma promoção de 5% no produto de {preco}R$ que deseja comprar.\nEntão no caso sua compra ficará no preço {porcentagem:.2f}R$')
-
-# desafio 9 
-# salario = int(input('Qual o seu sálario?: '))
-# salario2 = salario - (salario * 15 / 100)
-# print(f'Você receberá um aumento de 15% no seu sálario de {salario}R$. \nApartir de agora seu novo sálario será de {salario + salario2}R$')
-
-# desafio 10
-# celsius = float(input('Informe a temperatura agora em °C: '))
-# farenheit = (celsius * 9/5) + 32
-# print(f'A temperatura {celsius}°C, convertida em Farenheit é {farenheit}°F')
-
-#desafio 11
-dias = int(input('Quantos dias o carro ficou alugado?: '))
-km = float(input('Quantos KM foram rodados?: '))
-pagar = (dias * 60) + (0.15 * km)
-print(f'Sabendo que cada dia com o carro alugado custa R$60,\nE cada KM rodado com ele custa R$0.15 você precisa pagar R${pagar:.2f}')
+#desafio 17
+#não sei fazer NEM FUDENDO
+pass
