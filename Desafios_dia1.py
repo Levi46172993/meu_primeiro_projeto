b1a80890ca085aa1cf617e5d5856aaeb77e3a95f
Levi46172993 <Clevifranca7@gmail.com>
2025-05-12 18:00:49 -0300

    ...

--- Desafios_dia1.py
+++ Desafios_dia1.py
@@ -5,30 +5,40 @@
 # print(f'A parte inteira de {num} é {Inteiro} ')
 
 #desafio 13
-import math
-co = float(input('Comprimento do cateto oposto: '))
-ca = float(input('Comprimento do cateto adjacente: '))
-hi = math.hypot(ca , co)
-print(f'A hipotenusa vai medir {hi:.2f}')#Dá pra fazer desse jeito aí, ou pesquisar o calculo e aplicar no codigo
+# import math
+# co = float(input('Comprimento do cateto oposto: '))
+# ca = float(input('Comprimento do cateto adjacente: '))
+# hi = math.hypot(ca , co)
+# print(f'A hipotenusa vai medir {hi:.2f}')#Dá pra fazer desse jeito aí, ou pesquisar o calculo e aplicar no codigo
 
 #desafio 14
-#não sei fazer
-pass
-
+# from math import sin , cos , tan , radians
+# angulo = float(input('Digite o ângulo que deseja: '))
+# seno = sin(radians(angulo))
+# cosseno = cos(radians(angulo))
+# tangente = tan(radians(angulo))
+# print(f'O SENO de {angulo} é {seno:.2f}')
+# print(f'O COSSENO de {angulo} é {cosseno:.2f}')
+# print(f'O TANGENTE de {angulo} é {tangente:.2f}')
 # #desafio 15
-# print('O professor quer que um dos alunos apague o quadro.\nEntre Luiz, Giovanna, Pedro e lucas oq irá apagar o quadro vai  ser o...')
-# from random import randint 
-# pedro = 'Pedro'
-# luiz = 'Luiz'
-# guilherme = 'Guilherme'
-# lucas = 'Lucas'
-# print(randint(pedro , luiz , guilherme , lucas))
-pass
-
+# print('O professor quer que um dos alunos apague o quadro.')
+# from random import choice
+# n1 = input('Primeiro Aluno: ')
+# n2 = input('Segundo Aluno: ')
+# n3 = input('Terceiro Aluno: ')
+# n4 = input('Quarto Aluno: ')
+# lista = [n1 , n2 , n3 , n4]
+# resultado = choice(lista)
+# print(f'O aluno que irá ajudar o professor será o {resultado}')
 #desafio 16
-#não sei fazer 
-pass    
+# from random import shuffle
+# n1 = input('Primeiro Aluno: ')
+# n2 = input('Segundo Aluno: ')
+# n3 = input('Terceiro Aluno: ')
+# n4 = input('Quarto Aluno:')
+# lista = [n1 , n2 , n3 , n4]
+# shuffle(lista)
+# print(f'A ordem de aprensentação será:{lista}')
 
-#desafio 17
-#não sei fazer NEM FUDENDO
-pass
+# #desafio 17
+#Desafio de merda e professor de merda.A atividade só pode ser concluida no pycharm
