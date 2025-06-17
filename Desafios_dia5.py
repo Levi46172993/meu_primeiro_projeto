c4758dfbc54fd498c278f39591082c585744476e
Levi46172993 <Clevifranca7@gmail.com>
2025-06-17 17:12:05 -0300

    Update Desafios_dia5.py

--- Desafios_dia5.py
+++ Desafios_dia5.py
@@ -1,2 +1,12 @@
 #desafio 42 QEF (desafio 46 NOG)
-#C
+#Contagem de fogos
+# from time import sleep
+# for c in range(10 , -1, -1):
+#     print(c)
+#     sleep(1)
+# print('CABUUMMMM')
+
+#desafio 43 QEF (desafo 47 NOG)
+# for c in range(2 , 51 , 2):
+#     print(c , end=' ')
+# print('Acabou!')
