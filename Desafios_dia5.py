f1d798a14218a7f92d9cb133d0e2f645364b1a16
Levi46172993 <Clevifranca7@gmail.com>
2025-06-18 16:54:00 -0300

    Update Desafios_dia5.py

--- Desafios_dia5.py
+++ Desafios_dia5.py
@@ -6,7 +6,37 @@
 #     sleep(1)
 # print('CABUUMMMM')
 
-#desafio 43 QEF (desafo 47 NOG)
+#desafio 43 QEF (desafio 47 NOG)
+#Contagem de pares
 # for c in range(2 , 51 , 2):
 #     print(c , end=' ')
 # print('Acabou!')
+
+#desafio 44 QEF (desafio 48 NOG)
+#soma de numeros ímpares
+# cont = 0
+# soma = 0
+# for c in range(1 , 500 , 2):
+#     if c % 3 == 0:
+#         soma += c 
+#         cont += 1
+# print(f'A soma de todos os {cont} valores solicitados é {soma}')
+
+#desafio 45 QEF (desafio 49 NOG)
+#tabuada v2.0
+# num = int(input('Digite um número para ver sua tabuada: '))
+# for c in range(1 , 11):
+#     print(f'{num} X {c:2} = {num * c}')
+
+#desafio 46 QEF (desafio 50 NOG)
+#soma de números ímpares
+# cont = 0
+# soma = 0
+# for c in range(1 , 7):
+#     num = int(input(f'Digite o {c}° número: '))
+#     if num % 2 == 0:
+#         cont+= 1
+#         soma += num
+# print(f'A soma dos {cont} números PARES que me informou é {soma}')
+
+#desafio 47 QEF (desafio 51 NOG)
